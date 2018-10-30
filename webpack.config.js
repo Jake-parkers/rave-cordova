fdc2cbe5d0dada1d81b95f27b44b3159f4e0780a
Jake-parkers <joshuakwaku@outlook.com>
2018-10-30 19:58:28 +0100

    added start script

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: ['./src/rave.js', './src/misc.js'],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'rave.js',
+    path: '../../www/js'
   }
 };
